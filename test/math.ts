8d38f9f6625987475adceaa8168769509acf839f
jms <jm@audio.d3ck.net>
2018-09-19 16:40:48 +0200

    add nested math test

--- test/math.ts
+++ test/math.ts
@@ -11,6 +11,18 @@ export default {
                     width: "/call('mul', env('var1'), env('var2'))/"
                 }
             }
+        },
+        'pass numbers and units to nested calls': {
+            less: '@var1:10px;@var2:5;.class{width: @var1 * @var2 + @var2;}',
+            jss: {
+                '@env': {
+                    var1: '10px',
+                    var2: '5'
+                },
+                '.class': {
+                    width: "/call('add', call('mul', env('var1'), env('var2')), env('var2'))/"
+                }
+            }
         }
     }
 }
